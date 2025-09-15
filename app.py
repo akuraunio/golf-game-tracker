539202cbeafe27705e248ad6854020c2d0de1f9b
akuraunio <160453163+akuraunio@users.noreply.github.com>
2025-09-15 16:04:24 +0300

    set up basic structure of the app

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask import render_template, redirect, request
+import sqlite3
+from werkzeug.security import generate_password_hash
+import db
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/create_user", methods=["POST"])
+def create():
+    username = request.form["username"]
+    password1 = request.form["password1"]
+    password2 = request.form["password2"]
+    if password1 != password2:
+        return "Error: passwords do not match"
+    password_hash = generate_password_hash(password1)
+
+    try:
+        sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
+        db.execute(sql, [username, password_hash])
+    except sqlite3.IntegrityError:
+        return "Error: Username is already taken"
+
+    return "Account created"
